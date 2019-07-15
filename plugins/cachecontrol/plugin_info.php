9985933a812443ed7a9e1ee7faa1eb542818ad16
Martin Hecht <mrbaseman@gmx.de>
2019-07-15 21:50:14 +0200

    update cachecontrol plugin: activate it on backend and searchresults

--- plugins/cachecontrol/plugin_info.php
+++ plugins/cachecontrol/plugin_info.php
@@ -27,6 +27,13 @@ if(!defined('WB_PATH')) die(header('Location: ../../index.php'));
 /*
 Header: Version-History
 
+1.0.6 %(mrbaseman; 15 Jul, 2019)%
+        - use by default in search results, too
+        - bugfix: correct the previous change
+
+1.0.5 %(mrbaseman; 11 Jul, 2019)%
+        - activate by default in backend
+
 1.0.4 %(mrbaseman; 21 Feb, 2016)%
         - change filter type to page (last)
 
@@ -45,7 +52,7 @@ Header: Version-History
 
 $plugin_directory   = 'cachecontrol';
 $plugin_name        = 'Cache Control';
-$plugin_version     = '1.0.4';
+$plugin_version     = '1.0.6';
 $plugin_status      = 'beta';
 $plugin_platform    = '2.8';
 $plugin_author      = 'thorn, mrbaseman';
